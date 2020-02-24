6b5993334b6991f8d6be9a13c2d7cf1d25150f8c
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-24 17:00:22 +0200

    F*ck muha

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
-struct TBook
-{
+struct TBook {
     string title,
             author;
 };
+
 void create(char[]);
+
 void output(char[]);
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, ".866");
     char a[] = "file.txt";
     create(a);
@@ -21,32 +22,25 @@ int main()
 
 }
 
-void create(char a[])
-{
+void create(char a[]) {
     int n;
     TBook b;
-    cout << "Input n "; cin >> n;
+    cout << "Input n ";
+    cin >> n;
     cin.ignore();
     ofstream file(a, ios::binary);
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Input title "; getline(cin, b.title);
-        cout << "Input author "; getline(cin, b.author);
+    for (int i = 0; i < n; i++) {
+        cout << "Input title ";
+        getline(cin, b.title);
+        cout << "Input author ";
+        getline(cin, b.author);
     }
     file.close();
 
 }
 
-void output(char a[])
-{
+void output(char a[]) {
     TBook c;
     ifstream fil(a, ios::binary);
-    while (!fil.eof())
-    {
-        fil.read((char*)&c, sizeof(TBook));
-        if (!fil.eof())
-            cout << c.title << "  " << c.author << endl;
-
-    }
     fil.close();
 }
